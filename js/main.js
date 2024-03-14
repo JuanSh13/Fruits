cb36cab2e9fb6aee4b1e8365c68bb22b1bd4b96b
shjuanmsh <shjuanmsh@gmail.com>
2024-03-14 14:35:20 -0600

    finished header and section, responsive desing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+let menuToggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation-list');
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+});
+
+
+function imgSlider(anything) {
+  document.querySelector('.fruits').src = `assets/${anything}`;
+}
+
+function changeBgColor(color) {
+  const bg = document.querySelector('.bg');
+  bg.style.background = color;
+}
+
+
 let el = document.querySelectorAll('.thumb li');
 
 for(let i = 0; i < el.length; i++) {
